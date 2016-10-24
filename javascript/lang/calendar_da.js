28afc58f6a73943dd41f29186070ae4ac12a639c
Sander Hagenaars <sander.ha@hotmail.com>
2016-10-24 11:37:05 +0200

    added calendar_da

---
+++ javascript/lang/calendar_da.js
@@ -0,0 +1,5 @@
+$.CalendarWidget.setOptions({
+	startOnMonday: true,
+	calDaysLabels: ['Man', 'Tir', 'Ons', 'Tor', 'Fre', 'Lør', 'Søn'],
+	calMonthsLabels: ['Januar', 'Februar', 'Marts', 'April','Maj', 'Juni', 'Juli', 'August', 'September','Oktober', 'November', 'December']
+})
