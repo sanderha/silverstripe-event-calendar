4ec9e52d4a76c467c6cccf77d5eb99c2dca12a2a
Sander Hagenaars <sander.ha@hotmail.com>
2016-10-24 11:37:34 +0200

    Create date_da.js

---
+++ javascript/locale/date_da.js
@@ -0,0 +1,50 @@
+/**
+ * An Array of day names starting with Sunday.
+ * 
+ * @example dayNames[0]
+ * @result 'Sunday'
+ *
+ * @name dayNames
+ * @type Array
+ * @cat Plugins/Methods/Date
+ */
+Date.dayNames = ['Søndag', 'Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag'];
+
+/**
+ * An Array of abbreviated day names starting with Sun.
+ * 
+ * @example abbrDayNames[0]
+ * @result 'Sun'
+ *
+ * @name abbrDayNames
+ * @type Array
+ * @cat Plugins/Methods/Date
+ */
+Date.abbrDayNames = ['Søn', 'Man', 'Tir', 'Ons', 'Tor', 'Fre', 'Lør'];
+
+/**
+ * An Array of month names starting with Janurary.
+ * 
+ * @example monthNames[0]
+ * @result 'January'
+ *
+ * @name monthNames
+ * @type Array
+ * @cat Plugins/Methods/Date
+ */
+Date.monthNames = ['Januar', 'Februar', 'Marts', 'April', 'Maj', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'December'];
+
+/**
+ * An Array of abbreviated month names starting with Jan.
+ * 
+ * @example abbrMonthNames[0]
+ * @result 'Jan'
+ *
+ * @name monthNames
+ * @type Array
+ * @cat Plugins/Methods/Date
+ */
+Date.abbrMonthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'Maj', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec'];
+
+Date.firstDayOfWeek = 1;
+Date.format = 'dd.mmm.yyyy';
